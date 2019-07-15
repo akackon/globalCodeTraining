2b76410c7c9fc59e5f1dad0b5c6530b92ee7dc6f
Adrian Ackon <adrianackon@rocketmail.com>
2019-07-15 15:21:24 +0000

    Just added the fold function

---
+++ Week 2/Day 1/Advanced Python/fold.py
@@ -0,0 +1,7 @@
+def fold(x):
+    running_total = ""
+    for item in x :
+        running_total += item
+    print(running_total)
+# numbers = [1, 2, 3, 4, 5]
+fold(["Jasmine", "The Wise One", "The Third One"])
