0edc62e774d2c95835a36c4dad02fc37a0b8d782
Adrian Ackon <adrianackon@rocketmail.com>
2019-07-16 14:53:06 +0000

    Added the bomb

--- Week 2/Day 1/Advanced Python/fold.py
+++ Week 2/Day 1/Advanced Python/fold.py
@@ -1,7 +1,7 @@
 def fold(x):
-    running_total = ""
+    running_total = 0
     for item in x :
         running_total += item
     print(running_total)
-# numbers = [1, 2, 3, 4, 5]
-fold(["Jasmine", "The Wise One", "The Third One"])
+numbers = [1, 2, 3, 4, 5]
+fold(numbers )
